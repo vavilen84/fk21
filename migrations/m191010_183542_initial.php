98ea5213b644eaaf23563f30debeeffc1eefe7a4
Teplov <v.teplov@mobidev.biz>
2019-10-13 17:23:18 +0300

    initital

---
+++ migrations/m191010_183542_initial.php
@@ -0,0 +1,95 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191010_183542_initial
+ */
+class m191010_183542_initial extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('post', [
+            'id' => $this->primaryKey(),
+            'title' => $this->text(),
+            'content' => $this->text(),
+            'user_id' => $this->integer(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+        ]);
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'email' => $this->string(255),
+            'first_name' => $this->string(255),
+            'last_name' => $this->string(255),
+            'password' => $this->text(),
+            'salt' => $this->text(),
+            'role' => $this->smallInteger()->notNull()->defaultValue(10),
+            'type' => $this->smallInteger()->notNull()->defaultValue(10),
+            'about' => $this->text(),
+            'avatar' => $this->text(),
+            'pinterest_link' => $this->string(255),
+            'instagram_link' => $this->string(255),
+            'facebook_link' => $this->string(255),
+            'phone' => $this->string(255),
+            'skype' => $this->string(255),
+            'telegram' => $this->string(255),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+        ]);
+        $this->createTable('image', [
+            'id' => $this->primaryKey(),
+            'uuid' => $this->string(255)->unique(),
+            'ext' => $this->string(255),
+            'original_filename' => $this->string(255),
+            'title' => $this->string(255),
+            'description' => $this->text(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+        ]);
+        $this->createTable('gallery',[
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+        ]);
+        $this->createTable('user_gallery_image',[
+            'user_id' => $this->integer(),
+            'gallery_id' => $this->integer(),
+            'image_id' => $this->integer(),
+            'PRIMARY KEY(user_id, gallery_id, image_id)',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m191010_183542_initial cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191010_183542_initial cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ models/gii/User.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\models\gii;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $password
+ * @property string $salt
+ * @property int $role
+ * @property int $type
+ * @property string $about
+ * @property string $avatar
+ * @property string $pinterest_link
+ * @property string $instagram_link
+ * @property string $facebook_link
+ * @property string $phone
+ * @property string $skype
+ * @property string $telegram
+ * @property int $status
+ * @property int $created_at
+ * @property int $updated_at
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['password', 'salt', 'about', 'avatar'], 'string'],
+            [['role', 'type', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['email', 'first_name', 'last_name', 'pinterest_link', 'instagram_link', 'facebook_link', 'phone', 'skype', 'telegram'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'password' => 'Password',
+            'salt' => 'Salt',
+            'role' => 'Role',
+            'type' => 'Type',
+            'about' => 'About',
+            'avatar' => 'Avatar',
+            'pinterest_link' => 'Pinterest Link',
+            'instagram_link' => 'Instagram Link',
+            'facebook_link' => 'Facebook Link',
+            'phone' => 'Phone',
+            'skype' => 'Skype',
+            'telegram' => 'Telegram',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
